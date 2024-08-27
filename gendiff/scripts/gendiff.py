893e57abfccf53717e7def3a8251ec93a1eb8f92
qwerty <Kehsa754@gmail.com>
2024-08-27 16:12:53 +0300

    gendiff script

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument("first_file")
+    parser.add_argument("second _file")
+    args = parser.parse_args()
+    print(args.echo)
+
+
+if __name__ == '__main__':
+    main()
