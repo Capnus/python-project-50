c8d867212af6b8f19dc225dc6690f43f0e5f636b
capn <kehsa754@gmail.com>
2025-01-11 18:49:50 +0000

    n

---
+++ gendiff/__init__.py
@@ -0,0 +1,4 @@
+from gendiff.gendiff import generate_diff
+from gendiff.json import format_json
+from gendiff.plain import format_plain
+from gendiff.stylish import format_stylish
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,33 +1,5 @@
-import argparse
-
-from gendiff.gendiff import build_diff, parse_file
-from gendiff.json import format_json
-from gendiff.plain import format_plain
-from gendiff.stylish import format_stylish
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument('-f', '--format', help='set format of output',
-                        default='stylish', choices=['stylish', 'plain', 'json']
-                        )
-    return parser.parse_args()
-
-
-def generate_diff(first_file, second_file, format_type):
-    diff = build_diff(parse_file(first_file), parse_file(second_file))
-
-    if format_type == 'stylish':
-        return format_stylish(diff)
-    elif format_type == 'plain':
-        return format_plain(diff)
-    elif format_type == 'json':
-        return format_json(diff)
-
+from gendiff.gendiff import generate_diff
+from gendiff.cli import parse_args
 
 def main():
     args = parse_args()
